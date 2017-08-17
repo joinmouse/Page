0bf82934ec37973e3b0769c67ab9bfc499f09493
wuqi <908380434@qq.com>
2017-08-17 20:05:03 +0800

    3D

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# enterprise-page
-
-1.css和HTML_demo和项目
-
-2.jQuery_demo和项目
-
-3.Issues里为日常开发总结
-
-4.javascript_demo和小项目
+### Demos
--- 企业建站/企业建站 _模块化/src/js/main.js
+++
@@ -1,7 +0,0 @@
-define(['jquery','addmore','carousel','gotop'],
-	function($,AddMore,Carousel,GoTop){
-		Carousel.init($('.carousel'))
-		GoTop.init()
-		AddMore.init($('.btnMore'))
-        // Fn.init();
-})
